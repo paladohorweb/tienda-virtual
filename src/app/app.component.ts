4b57fc466b97b10c6529363dc3c6e23a6de2356a
Jorge Gomez <paladohor@gmail.com>
2025-03-29 05:09:34 -0500

    creacion footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from "./pages/auth/login/login.component"; // ✅ Importamos el Navbar standalone
+import { LoginComponent } from "./pages/auth/login/login.component";
+import { FooterComponent } from "./components/footer/footer.component"; // ✅ Importamos el Navbar standalone
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet, NavbarComponent, LoginComponent] // ✅ Lo agregamos en imports
+  imports: [RouterOutlet, NavbarComponent, LoginComponent, FooterComponent] // ✅ Lo agregamos en imports
+ // ✅ Lo agregamos en imports
  // ✅ Lo agregamos en imports
 })
 export class AppComponent {
